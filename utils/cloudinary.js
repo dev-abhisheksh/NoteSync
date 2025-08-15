cab3d4c50edb18b6db8b45e0377ece9862ad91a5
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-08-15 11:48:18 +0530

    changed typo in cloudinary file

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOULD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API,
     api_secret: process.env.CLOUDINARY_SECRET
 });
